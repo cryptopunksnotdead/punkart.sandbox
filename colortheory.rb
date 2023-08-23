3ee57076bdbf92eb6e4821f78069177ef2a625a2
Gerald Bauer <gerald.bauer@gmail.com>
2023-08-23 18:14:30 +0200

    up

---
+++ colortheory.rb
@@ -0,0 +1,45 @@
+####
+#  a little script on color theory / basics
+
+require 'pixelart'
+
+## note: MUST add alpha channel!!!
+r = 0xff000000
+g = 0x00ff0000
+b = 0x0000ff00
+
+colors = {
+  '000__black':   0xff,                # k(ey)
+  '100__red':     r+0xff,              # r 
+  '110__yellow_(r+g)':  r+g+0xff,      # y
+  '010__green':   g+0xff,              # g
+  '011__cyan_(g+b)':     g+b+0xff,     # c
+  '001__blue':    b+0xff,              # b
+  '101__magenta_(r+b)':  r+b+0xff,     # m
+  '111__white_(r+g+b)': r+g+b+0xff,    # w
+}
+
+colors.each_with_index do |(name, color), i|
+  img = Image.new( 24, 24, color )
+  print "  #{i+1} - "
+  print '%-24s' % name
+  print "  "
+  print Color.fmt( color )
+  print "\n"
+  img.save( "./tmp2/#{i+1}-#{name}.png" )
+end
+
+
+
+puts "bye"
+
+__END__
+  1 - 000  black                #000000 / rgb(  0   0   0) - hsl(  0°   0%   0%) - hsv(  0°   0%   0%)           - BLACK
+  2 - 100  red                  #ff0000 / rgb(255   0   0) - hsl(  0° 100%  50%) - hsv(  0° 100% 100%)
+  3 - 110  yellow (r+g)         #ffff00 / rgb(255 255   0) - hsl( 60° 100%  50%) - hsv( 60° 100% 100%)
+  4 - 010  green                #00ff00 / rgb(  0 255   0) - hsl(120° 100%  50%) - hsv(120° 100% 100%)
+  5 - 011  cyan (g+b)           #00ffff / rgb(  0 255 255) - hsl(180° 100%  50%) - hsv(180° 100% 100%)
+  6 - 001  blue                 #0000ff / rgb(  0   0 255) - hsl(240° 100%  50%) - hsv(240° 100% 100%)
+  7 - 101  magenta (r+b)        #ff00ff / rgb(255   0 255) - hsl(300° 100%  50%) - hsv(300° 100% 100%)
+  8 - 111  white (r+g+b)        #ffffff / rgb(255 255 255) - hsl(  0°   0% 100%) - hsv(  0°   0% 100%)           - WHITE
+
--- samples.rb
+++ samples.rb
@@ -4,7 +4,8 @@ require 'punks'
 
 
 
-def make_composite( glob, base: )
+def make_composite( glob, zoom: 8, name: nil, 
+                       base:  )
   files = Dir.glob( glob )
   puts "   #{files.size} file(s)"
   
@@ -18,7 +19,6 @@ def make_composite( glob, base: )
 
   files.each do |file|
     basedir  = File.basename( File.dirname( file ))  ## e.g. f or m or such expected
-    subdir   = File.basename( File.dirname(File.dirname( file )))
     basename = File.basename( file, File.extname( file ))
 
     hair = Image.read( file )
@@ -30,7 +30,9 @@ def make_composite( glob, base: )
     punk.compose!( hair )
     
     ## save individual as 8x?
-    punk.zoom(8).save( "./tmp/#{subdir}_#{basename}-#{basedir}@8x.png" )
+    slug = name ?  basename : name.call( basename )
+    
+    punk.zoom( zoom ).save( "./tmp/#{slug}@#{zoom}x.png" )
     composite << punk 
   end
 
@@ -39,27 +41,37 @@ end
 
 
 base = Punk::Image.generate( 'Ghost'  )    
-m = make_composite( '../punkart.blocks.hair/hair/m/*.png', base: base )
+m = make_composite( '../punkart.blocks.hair/hair/m/*.png', 
+                        base: base,
+                        name: ->(name) { "hair_#{name}-m" } )
 m.save( "./tmp/hair_m.png")
 m.zoom(4).save( "./tmp/hair_m@4x.png")
 
 base = Punk::Image.generate( 'Orc', 'Eye Mask', 'Goat', 'Gold Chain'  )    
-m = make_composite( '../punkart.blocks.hair/hair/m/*.png', base: base )
+m = make_composite( '../punkart.blocks.hair/hair/m/*.png', 
+                         base: base,
+                         name: ->(name) { "hair_#{name}-m_orc" }  )
 m.save( "./tmp/orc_hair_m.png")
 m.zoom(4).save( "./tmp/orc_hair_m@4x.png")
 
 base = Punk::Image.generate( 'Ghost Female' )    
-f = make_composite( '../punkart.blocks.hair/hair/f/*.png', base: base )
+f = make_composite( '../punkart.blocks.hair/hair/f/*.png', 
+                             base: base,
+                             name: ->(name) { "hair_#{name}-f" } )
 f.save( "./tmp/hair_f.png")
 f.zoom(4).save( "./tmp/hair_f@4x.png")
 
 base = Punk::Image.generate( 'Orc Female' )    
-f = make_composite( '../punkart.blocks.hair/hair/f/*.png', base: base )
+f = make_composite( '../punkart.blocks.hair/hair/f/*.png', 
+                                 base: base,
+                                 name: ->(name) { "hair_#{name}-f_orc" } )
 f.save( "./tmp/orc_hair_f.png")
 f.zoom(4).save( "./tmp/orc_hair_f@4x.png")
 
 base = Punk::Image.generate( 'Female3', 'Clown Nose' )    
-f = make_composite( '../punkart.blocks.hair/hair/f/*.png', base: base )
+f = make_composite( '../punkart.blocks.hair/hair/f/*.png', 
+                                   base: base,
+                                   name: ->(name) { "hair_#{name}-f_female3" } )
 f.save( "./tmp/female3_hair_f.png")
 f.zoom(4).save( "./tmp/female3_hair_f@4x.png")
 
@@ -67,15 +79,29 @@ f.zoom(4).save( "./tmp/female3_hair_f@4x.png")
 ####
 # basic
 base = Punk::Image.generate( 'Ghost'  )    
-m = make_composite( '../punkart.blocks.hair/basic/m/*.png', base: base )
+m = make_composite( '../punkart.blocks.hair/basic/m/*.png', 
+                                  base: base,
+                                  name: ->(name) { "basic_#{name}-m" } )
 m.save( "./tmp/basic_m.png")
 m.zoom(4).save( "./tmp/basic_m@4x.png")
 
 base = Punk::Image.generate( 'Ghost Female' )    
-f = make_composite( '../punkart.blocks.hair/basic/f/*.png', base: base )
+f = make_composite( '../punkart.blocks.hair/basic/f/*.png', 
+                                 base: base,
+                                 name: ->(name) { "basic_#{name}-f" } )
 f.save( "./tmp/basic_f.png")
 f.zoom(4).save( "./tmp/basic_f@4x.png")
 
+
+
+###
+# beards - basic
+base = Punk::Image.generate( 'Ghost'  )    
+m = make_composite( '../punkart.blocks.hair/beard/*.png', 
+                                  base: base,
+                                  name: ->(name) { "beardbasic_#{name}" } )
+m.save( "./tmp/beardbasic.png")
+m.zoom(4).save( "./tmp/beardbasic@4x.png")
 
 
 puts "bye"
